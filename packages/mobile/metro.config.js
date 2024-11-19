f092631ded6948b4f5031f252be282466a4b2d6f
Henry Allen <31718268+hmallen99@users.noreply.github.com>
2024-11-18 19:02:33 -0500

    Initialize React Native App (#1)
    * Add package.json
    
    * Init RN Test app
    
    * Update podfile
    
    * Update Metro Config

---
+++ packages/mobile/metro.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const { makeMetroConfig } = require("@rnx-kit/metro-config");
+module.exports = makeMetroConfig({
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  watchFolders: [
+    path.resolve(__dirname, "../../node_modules"),
+    path.resolve(__dirname, "../../node_modules/ir-engine-app"),
+  ],
+});
